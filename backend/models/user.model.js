2823e166e7c345ed475a1c7d02e7e80b6cac4479
tusharsharma2822 <tusharsharma2822@gmail.com>
2025-01-16 21:28:36 +0530

    Logic for the creation of the user

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,42 +1,39 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
-    email: {
+    email:{
         type: String,
         required: true,
         unique: true,
         trim: true,
         lowercase: true,
-        minLength: [ 6, 'Email must be at least 6 characters long' ],
-        maxLength: [ 50, 'Email must not be longer than 50 characters' ]
+        minLength: [6, "Email must contain atleast 6 letters"],
+        maxLength: [50, "Email must contain maxium 50 letters"]
     },
-
-    password: {
+    password:{
         type: String,
         select: false,
     }
 })
 
-
 userSchema.statics.hashPassword = async function (password) {
-    return await bcrypt.hash(password, 10);
+    return await bcrypt.hash(password,10);
 }
 
 userSchema.methods.isValidPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.generateJWT = function () {
+userSchema.methods.generateJWT = function() {
     return jwt.sign(
-        { email: this.email },
+        {email: this.email},
         process.env.JWT_SECRET,
-        { expiresIn: '24h' }
+        { expiresIn: '24h'}
     );
 }
 
-
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model('user',userSchema);
 
 export default User;
