6c2d5c32d6fe32a8791ecba3c9a5677534551f30
tusharsharma2822 <tusharsharma2822@gmail.com>
2025-01-21 22:23:34 +0530

    update user.route.js to define user-related routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -8,6 +8,12 @@ router.post('/register',
     body('email').isEmail().withMessage('Email must be a valid email address'),
     body('password').isLength({min:3}).withMessage('Password must be atleast 3 character long'),
     userController.createUserController
+);
+
+router.post('/login',
+    body('email').isEmail().withMessage('Email must be a valid email address'),
+    body('password').isLength({min:3}).withMessage('Password must be atleast 3 character long'),
+    userController.loginController
 )
 
 export default router;
