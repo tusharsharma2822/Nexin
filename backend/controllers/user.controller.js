3aaf6bc73483b4f87babb34f89fe90dc833929eb
tusharsharma2822 <tusharsharma2822@gmail.com>
2025-01-21 22:22:30 +0530

    Update user.controller.js to manage user-related request

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -19,3 +19,38 @@ export const createUserController = async (req,res) => {
         res.status(400).send(error.message)
     }
 }
+
+export const loginController = async (req,res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({ email }).select('+password')
+
+        if(!user){
+            return res.status(400).json({
+                errors: 'Invalid Credentials'
+            })
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                errors: 'Invalid Credentials'
+            })
+        }
+
+        const token = await user.generateJWT();
+
+        return res.status(201).json({ user,token })
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
