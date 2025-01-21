cbdce27fe0a9d59a3c6385fd3f3d0d7f05c2c6ce
tusharsharma2822 <tusharsharma2822@gmail.com>
2025-01-21 14:07:25 +0530

    Update app.js to improve backend functionality and fix minor issues

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import connectToDB from "./db/db.js";
+import userRoutes from './routes/user.route.js'
 
 connectToDB();
 
@@ -13,8 +14,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/users', userRoutes)
+
+
 app.get('/', (req,res) => {
     res.send('Hello, World!');
 })
+
+
 
 export default app;
