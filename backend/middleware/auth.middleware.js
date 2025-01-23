0fed7d6161f5e5c9cf52fe3a54621242e820b0d5
tusharsharma2822 <tusharsharma2822@gmail.com>
2025-01-23 21:37:17 +0530

    improve token validation in auth.middleware.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import redisClient from '../services/redis.service.js';
 
 export const authUser = async (req,res,next) => {
     try{
@@ -6,6 +7,13 @@ export const authUser = async (req,res,next) => {
 
         if(!token){
             return res.status(401).send({error: 'Please authenticate the user'});
+        }
+
+        const isBlackListed = await redisClient.get(token);
+
+        if(isBlackListed){
+            res.cookie('token', '');
+            return res.status(400).send({ error: 'Unauthorized User'});
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
