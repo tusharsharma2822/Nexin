419a88c7a509495106776cb36831476117660b95
tusharsharma2822 <tusharsharma2822@gmail.com>
2025-01-23 10:38:00 +0530

    improve token validation in auth.middleware.js

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export const authUser = async (req,res,next) => {
+    try{
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+        if(!token){
+            return res.status(401).send({error: 'Please authenticate the user'});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }catch(error){
+        return res.status(401).send({
+            error: 'Please Authenticate the user'
+        })
+    }
+}
