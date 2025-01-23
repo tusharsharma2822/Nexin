bdfb60ad295b972bf84002bd7a761f1960d4b5fa
tusharsharma2822 <tusharsharma2822@gmail.com>
2025-01-23 10:31:56 +0530

    Update user.controller.js to manage user-related request

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { validationResult } from 'express-validator';
 import userModel from '../models/user.model.js'
 import * as userService from '../services/user.service.js';
+import redisClient from '../services/redis.service.js';
 
 export const createUserController = async (req,res) => {
     const errors = validationResult(req);
@@ -52,5 +53,28 @@ export const loginController = async (req,res) => {
 
     } catch (error) {
         return res.status(400).json(error.message);
+    }
+}
+
+export const profileController = async (req,res) => {
+    console.log(req.user)
+
+    return res.status(200).json({
+        user: req.user
+    })
+}
+
+export const logoutController = async (req,res) => {
+    try {
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+        redisClient.set(token, 'logout', 'EX', 60*60*24);
+
+        res.status(200).json({
+            message: 'Logged Out Successfully'
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send(error.message)
     }
 }
