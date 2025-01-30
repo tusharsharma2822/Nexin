f61c33eeca416c0e42d7fcf5ff24db20dc2a8f3e
Tushar Sharma <tusharsharma2822@gmail.com>
2025-01-30 13:52:57 +0530

    Containing all the routes of the Application

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../screens/Home'
+import Login from '../screens/Login'
+import Register from '../screens/Register'
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />}  />
+            <Route path='/register' element={<Register />}  />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
