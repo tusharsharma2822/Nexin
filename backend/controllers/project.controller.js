457e5127e6f3cb41506f54e6af7fcba08effb65b
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-03 20:37:40 +0530

    Creating project.controller.js to manage project related requests

---
+++ backend/controllers/project.controller.js
@@ -0,0 +1,7 @@
+import ProjectModel from "../models/project.model.js";
+import projectService from "../services/project.service.js";
+import { validationResult } from 'express-validator';
+
+export const createProjectController = async (req, res) => {
+    
+}
