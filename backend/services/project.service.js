9649395f7b920f5d96d10d9b0f8749822cf391b3
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-03 20:36:38 +0530

    Creating project.service.js to add some project related service

---
+++ backend/services/project.service.js
@@ -0,0 +1,18 @@
+import projectModel from '../models/project.model.js';
+
+export const createProject = async ({name, userId}) => {
+    if(!name){
+        throw new Error('Name is required');
+    }
+
+    if(!userId){
+        throw new Error('UserId is required');
+    }
+
+    const project = await projectModel.create({
+        name,
+        users: [ userId ]
+    })
+
+    return project;
+}
