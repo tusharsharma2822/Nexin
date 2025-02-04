2bd9fed42adab22712833cf8d3c97d03237f4307
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-04 09:52:21 +0530

    Updating the project.controller.js to enhance the project related requests

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -1,7 +1,28 @@
 import ProjectModel from "../models/project.model.js";
-import projectService from "../services/project.service.js";
+import * as projectService from "../services/project.service.js";
 import { validationResult } from 'express-validator';
+import userModel from '../models/user.model.js';
 
 export const createProjectController = async (req, res) => {
-    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.message });
+    }
+
+    try {
+
+        const { name } = req.body;
+
+        const loggedInUser = await userModel.findOne({ email: req.user.email })
+        
+        const userId = loggedInUser._id;
+
+        const newProject = await projectService.createProject({ name, userId });
+
+        res.status(201).json(newProject);
+
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
 }
