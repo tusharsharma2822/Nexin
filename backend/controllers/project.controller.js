9b504436fb57d4b696690ac20f3c4f7286843a40
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-05 15:14:20 +0530

    update project.controller.js to improve project related routes

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -1,29 +1,77 @@
 import ProjectModel from "../models/project.model.js";
 import * as projectService from "../services/project.service.js";
-import { validationResult } from 'express-validator';
-import userModel from '../models/user.model.js';
+import { validationResult } from "express-validator";
+import userModel from "../models/user.model.js";
 
 export const createProjectController = async (req, res) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.message });
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.message });
+  }
 
-    try {
+  try {
+    const { name } = req.body;
 
-        const { name } = req.body;
+    const loggedInUser = await userModel.findOne({ email: req.user.email });
 
-        const loggedInUser = await userModel.findOne({ email: req.user.email })
-        
-        const userId = loggedInUser._id;
+    const userId = loggedInUser._id;
 
-        const newProject = await projectService.createProject({ name, userId });
+    const newProject = await projectService.createProject({ name, userId });
 
-        res.status(201).json(newProject);
+    res.status(201).json(newProject);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send(err.message);
+  }
+};
 
-    } catch (err) {
-        console.log(err);
-        return res.status(400).send(err.message);
-    }
-}
+export const getAllProject = async (req, res) => {
+  try {
+    const loggedInUser = await userModel.findOne({
+      email: req.user.email,
+    });
+
+    const allUsersProject = await projectService.getAllProjectByUserId({
+      userId: loggedInUser._id,
+    });
+
+    return res.status(200).json({
+      projects: allUsersProject,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+export const addUserToProject = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    
+    const { projectId, users } = req.body;
+    
+    const loggedInUser = await userModel.findOne({
+        email: req.user.email
+    }) 
+
+    const project = await projectService.addUsersToProject({
+        projectId,
+        users,
+        userId: loggedInUser._id
+    })
+
+    return res.status(200).json({
+        project,
+    })
+
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
