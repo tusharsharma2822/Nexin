d63f523dea76a3448e1d2cdbbd050f1949238d28
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-05 15:12:58 +0530

    Update project.routes.js to enhance project-related routes

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -11,4 +11,17 @@ router.post('/create',
     projectController.createProjectController
 );
 
+router.get('/all',
+    authMiddleware.authUser,
+    projectController.getAllProject
+)
+
+router.put('/add-user',
+    authMiddleware.authUser,
+    body('projectId').isString().withMessage('Project ID is required'),
+    body('users').isArray({ min: 1 }).withMessage('Users must be an array with at least one user').bail()
+        .custom((users) => users.every(user => typeof user === 'string')).withMessage('Each user must be a string'),
+    projectController.addUserToProject
+)
+
 export default router;
