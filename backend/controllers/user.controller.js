094b5e7fe1b6ca9147d445353032f73ea221a84c
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-06 22:51:27 +0530

    update user.controller.js to improve user-related request

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -82,3 +82,22 @@ export const logoutController = async (req,res) => {
         return res.status(400).send(error.message)
     }
 }
+
+export const getAllUsersController = async (req, res) => {
+    try {
+
+        const loggedInUser = await userModel.findOne({
+            email: req.user.email
+        })
+
+        const allUsers = await userService.getAllUsers({   userId: loggedInUser._id    })
+
+        return res.status(200).json({
+            users: allUsers
+        })
+        
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({   error: err.message   })
+    }
+}
