6fa4548168fa53cddd00114e8a8f15f73553709f
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-06 22:50:25 +0530

    update user.service.js to improve the user-related-services

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -14,3 +14,10 @@ export const createUser = async({email,password}) => {
 
     return user;
 }
+
+export const getAllUsers = async ({  userId   }) => {
+    const users = await userModel.find({
+        _id: {  $ne: userId  }
+    });
+    return users;   
+}
