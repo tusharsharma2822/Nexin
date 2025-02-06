e46f4c8fa0e293825f020aff39b3b8e1de4174f7
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-06 22:49:39 +0530

    update update user.routes.js to improve user-related-routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -21,4 +21,6 @@ router.get('/profile', authMiddleware.authUser ,userController.profileController
 
 router.get('/logout', authMiddleware.authUser, userController.logoutController)
 
+router.get('/all', authMiddleware.authUser, userController.getAllUsersController)
+
 export default router;
