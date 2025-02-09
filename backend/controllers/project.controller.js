bf00d04ea0fa8fc6fd6ff19d8737c7d4d1d373da
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-09 14:54:02 +0530

    update project.controller.js to enhance project related request

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -75,3 +75,23 @@ export const addUserToProject = async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 };
+
+export const getProjectById = async (req, res) => {
+
+  const {projectId }= req.params;
+
+  try{
+
+    const project = await projectService.getProjectById({ projectId });
+
+    return res.status(200).json({
+      project
+    })
+
+  }catch(err){
+    console.log(err);
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+}
