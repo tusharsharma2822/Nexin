7077a6603687c81aaacb6309172d692debf85334
Tushar Sharma <tusharsharma2822@gmail.com>
2025-02-27 11:16:04 +0530

    Frontend files

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,13 @@ import express from "express";
 import connectToDB from './db/db.js';
 import userRoutes from "./routes/user.routes.js";
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 connectToDB()
 
 const app = express();
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('dev'));
--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from '../config/axios';
 
 const Register = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  function submitHandler(e) {
+    e.preventDefault()
+
+    axios.post('/users/register', {
+      email,
+      password
+    }).then((res) => {
+      console.log(res.data);
+      navigate('/home');
+    }).catch((err) => {
+      console.log(err.response.data);
+    })
+  }
+
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-950'>
       <div className='bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md transition-transform transform hover:scale-105 hover:shadow-xl'>
         <h2 className='text-3xl font-bold text-center text-white mb-6'>Register to Chat</h2>
-        <form>
+        <form onSubmit={submitHandler}>
           <div className='mb-4'>
             <label className='block text-white mb-2' htmlFor='email'>Email</label>
             <input 
+              onChange={(e) => setEmail(e.target.value)}
               type='email'
               id='email'
               className="w-full p-3 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-neon-green"
@@ -19,6 +41,7 @@ const Register = () => {
           <div className='mb-4'>
             <label className='block text-white mb-2'>Password</label>
             <input 
+              onChange={(e) => setPassword(e.target.value)}
               type='password'
               id='password'
               className='w-full p-3 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-neon-green'
