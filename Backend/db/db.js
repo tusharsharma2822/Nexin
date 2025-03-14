25b3d10a28b5f876e60f05e2371f0eb6688619b9
Tushar Sharma <tusharsharma2822@gmail.com>
2025-03-14 15:28:17 +0530

    deleted

--- Backend/db/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-function connectToDB() {
-    mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log("Connected to the MongoDB");
-    })
-}
-
-export default connectToDB;
