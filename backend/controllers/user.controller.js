ee3eb38db10274dd2512b49441a1cde8a8e1022f
Tushar Sharma <tusharsharma2822@gmail.com>
2025-03-26 15:01:59 +0530

    update user.controller.js to handle user-related-request

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,90 @@
+import { validationResult } from "express-validator";
+import userModel from "../models/user.model.js";
+import * as userService from "../services/user.service.js";
+import redisClient from "../services/redis.service.js";
+
+export const createUserController = async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const user = await userService.createUser(req.body);
+        
+        const token = await user.generateToken();
+
+        return res.status(201).json({user,token});
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err.message);
+    }
+    
+}
+
+export const loginController = async (req, res) => {
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        
+        const {email, password} =  req.body;
+
+        const user = await userModel.findOne({email}).select('+password');
+
+        if(!user){
+            return res.status(401).json({
+                message: 'Invalid Credentials'
+            })
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        if(!isMatch) {
+            return res.status(401).json({
+                message: 'Invalid Credentials'
+            })
+        }
+
+        const token = await user.generateToken();
+
+        return res.status(200).json({ user, token });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send(error.message);
+    }
+
+}
+
+export const profileController = async (req, res) => {
+    return res.status(200).json({
+        user: req.user
+    });
+}
+
+export const logoutController = async (req, res) => {
+    try {
+
+        const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+        redisClient.set(token, 'logout', 'EX', 60*60*24);
+
+        res.status(200).json({
+            message: "Logged Out Successfully"
+        });
+        
+    } catch(err) {
+
+        return res.status(400).send(err.message);
+        
+    }
+}
